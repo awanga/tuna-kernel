5b13db2d992fde68c9b711ff711fe9d64e1b617f
Alfred Wanga <awanga@gmail.com>
2015-05-20 21:24:23 -0400

    OMAP4: backport thermal framework

--- arch/arm/plat-omap/include/plat/temperature_sensor.h
+++ arch/arm/plat-omap/include/plat/temperature_sensor.h
@@ -56,7 +56,7 @@ static inline void omap_temp_sensor_resume_idle(void) { }
 static inline void omap_temp_sensor_prepare_idle(void) { }
 #endif
 
-#ifdef CONFIG_OMAP_DIE_TEMP_SENSOR
+#ifdef CONFIG_OMAP4_DIE_TEMP_SENSOR
 void omap_temp_sensor_idle(int idle_state);
 #else
 static inline void omap_temp_sensor_idle(int idle_state) { }
